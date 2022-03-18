ab42e1c75302fd6bbd2481f48d83df3e6edf7f3e
hugosenari <hugosenari@gmail.com>
2022-03-18 17:25:18 -0300

    chore: re index globby

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."globby-master".dir   = "master";
-  inputs."globby-master".owner = "nim-nix-pkgs";
-  inputs."globby-master".ref   = "master";
-  inputs."globby-master".repo  = "globby";
-  inputs."globby-master".type  = "github";
-  inputs."globby-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."globby-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."globby-0_1_0".dir   = "0_1_0";
-  inputs."globby-0_1_0".owner = "nim-nix-pkgs";
-  inputs."globby-0_1_0".ref   = "master";
-  inputs."globby-0_1_0".repo  = "globby";
-  inputs."globby-0_1_0".type  = "github";
-  inputs."globby-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."globby-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
